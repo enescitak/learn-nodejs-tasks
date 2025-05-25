06164725f385387e2ee2f321342057603ed26ae5
enescitak <enescitak@gmail.com>
2025-05-25 13:22:36 +0300

    Complete Clean Blog project - Add modern banner design and responsive layout

---
+++ odev-8-clean-blog-2/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.listen(2100, () => {
+  console.log('Sunucu 2100 portunda başlatıldı');
+}); 
